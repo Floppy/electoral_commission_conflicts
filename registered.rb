cbbe03878fb0971fa4ffe2ebfe40b03ba619981a
James Smith <james@floppy.org.uk>
2015-01-09 14:04:22 +0000

    remove common words and minor parties

--- registered.rb
+++ registered.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require 'csv'
 
 parties = CSV.parse(File.read('UK-Political-Parties/data/parties.csv'), headers: true)
-names = parties.map{|x| x['Entity name']}
+names = parties.map{|x| x["Entity type"] == "Political Party" ? x['Entity name'] : nil}.compact
 
 # Process similarity of names
 
@@ -20,11 +20,13 @@ CSV.open("registered.csv", "wb") do |csv|
   names.each do |name|
     names.each do |comparison|
       if name != comparison
+        a = name.gsub("Party", "").gsub("The", "")
+        b = comparison.gsub("Party", "").gsub("The", "")
         csv << [
-          name,
-          comparison,
-          name.similar(comparison),
-          Levenshtein.distance(name,comparison)
+          a,
+          b,
+          a.similar(b),
+          Levenshtein.distance(a,b)
         ]
       end
     end
